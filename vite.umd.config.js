b8325bc2c10b71dbe14229d721edd8c30db8eb3f
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-05-06 10:57:30 +0530

    chore: updated vite config

--- vite.umd.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import { commonConfig } from "./common.config";
-import packageFile from "./package.json";
-
-export default defineConfig((_configEnv) => ({
-  ...commonConfig("src", "umd", packageFile),
-}));
